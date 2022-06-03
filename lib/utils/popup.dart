cb5c01dd866a26150b89f6a4542264c1b0cb57fc
HonmaMeikodesu <whonmameikom@gmail.com>
2022-06-04 00:43:51 +0800

    refine showToast, now support overlay implements, polyfilled with Navigator

--- lib/utils/popup.dart
+++ lib/utils/popup.dart
@@ -22,7 +22,72 @@ enum toastType {
   error,
 }
 
-void showToast({required BuildContext context, required String content, toastType type = toastType.success, int? closeAfter}) {
+class _ToastPopup extends StatefulWidget {
+  final Color containerBgColor;
+  final Icon icon;
+  final Color textColor;
+  final String content;
+  const _ToastPopup({Key? key, required this.content, required this.containerBgColor, required this.icon, required this.textColor}): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ToastPopupState();
+}
+
+class _ToastPopupState extends State<_ToastPopup> with TickerProviderStateMixin {
+
+  late AnimationController controller;
+
+  @override
+  initState(){
+    controller = AnimationController(
+        vsync: this,
+        duration: const Duration( milliseconds: 100),
+    );
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        left: 80,
+        right: 80,
+        top: 20,
+        child: SlideTransition(
+          position: Tween(begin: Offset(0, -2), end: Offset(0, 0)).animate(controller),
+          child: Container(
+            height: 48,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: widget.containerBgColor,
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+            ),
+            child: Flex(
+              mainAxisSize: MainAxisSize.min,
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                widget.icon,
+                DefaultTextStyle(
+                  style: TextStyle(color: widget.textColor),
+                  child: Text(widget.content),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+void showToast({required BuildContext context, required String content, toastType type = toastType.success, required int closeAfter}) {
   Icon icon;
   Color containerBgColor;
   Color textColor;
@@ -37,45 +102,60 @@ void showToast({required BuildContext context, required String content, toastTyp
       containerBgColor = Color.fromRGBO(248, 148, 139, 0.8);
       textColor = Colors.black87;
   }
-  showGeneralDialog(
+  OverlayState? overlayContainer = Overlay.of(context);
+  if ( overlayContainer is OverlayState ) {
+    OverlayEntry dialog = OverlayEntry(
+        builder: (context) {
+      return _ToastPopup(content: content, containerBgColor: containerBgColor, icon: icon, textColor: textColor);
+    });
+    overlayContainer.insert(dialog);
+    if (closeAfter is int) {
+      Future.delayed(Duration(seconds: closeAfter), () {
+        dialog.remove();
+      });
+    }
+  } else {
+    showGeneralDialog(
       context: context,
       barrierDismissible: false,
       pageBuilder: (pageBuildercontext, anim1, anim2) {
-          if (closeAfter is int) {
-            Future.delayed(Duration(seconds: closeAfter), () {
-                Navigator.of(context).maybePop();
-            });
-          }
-          return SafeArea(child: Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: containerBgColor,
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-              ),
-              child: Flex(
-                mainAxisSize: MainAxisSize.min,
-                direction: Axis.horizontal,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  icon,
-                  DefaultTextStyle(
-                    style: TextStyle(color: textColor),
-                    child: Text(content),
-                  )
-                ],
-              ),
+        if (closeAfter is int) {
+          Future.delayed(Duration(seconds: closeAfter), () {
+            Navigator.of(context).maybePop();
+          });
+        }
+        return SafeArea(child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: containerBgColor,
+              borderRadius: BorderRadius.all(Radius.circular(8)),
             ),
-          ));
+            child: Flex(
+              mainAxisSize: MainAxisSize.min,
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                DefaultTextStyle(
+                  style: TextStyle(color: textColor),
+                  child: Text(content),
+                )
+              ],
+            ),
+          ),
+        ));
       },
       transitionBuilder: (context, anim1, anim2, child) {
+
         return SlideTransition(
-          position: Tween(begin: Offset(0, -1), end: Offset(0, 0.01)).chain(CurveTween(curve: Curves.bounceIn)).animate(anim1),
+          position: anim1.drive(Tween(begin: Offset(0, -1), end: Offset(0, 0.01)).chain(CurveTween(curve: Curves.bounceIn))),
           child: child,
         );
       },
-  );
+    );
+  }
 }
 
 void showMessage({required BuildContext context,required String title, required void callback()}) {
