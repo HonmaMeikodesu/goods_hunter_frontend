3488d829241706a6c50ece179b908f032fd1958b
HonmaMeiko <17mwang2@stu.edu.cn>
2022-06-14 01:03:35 +0800

    bug fix for hunterPub tabBar

--- lib/routers/hunterPub.dart
+++ lib/routers/hunterPub.dart
@@ -20,29 +20,25 @@ Map<String, String> hunterRoundTables = {
 class PubTab extends StatelessWidget {
   final String imageUrl;
   final String content;
-  final void Function() onClick;
 
-  const PubTab({Key? key, required this.imageUrl, required this.content, required this.onClick})
+  const PubTab({Key? key, required this.imageUrl, required this.content })
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onClick,
-      child: Flex(
-        direction: Axis.horizontal,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 54),
-              child: Image.asset(imageUrl)
-          ),
-          Text(
-            content,
-          )
-        ],
-      ),
+    return Flex(
+      direction: Axis.horizontal,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 54),
+            child: Image.asset(imageUrl)
+        ),
+        Text(
+          content,
+        )
+      ],
     );
   }
 }
@@ -58,6 +54,7 @@ class _HunterPubRouteState extends State with TickerProviderStateMixin {
   Animation<double>? overlayAnimation;
   late AnimationController animationController;
   late CurvedAnimation curvedAnimation;
+  late TabController tabController;
 
   @override
   void initState() {
@@ -66,6 +63,7 @@ class _HunterPubRouteState extends State with TickerProviderStateMixin {
         vsync: this,
     );
     curvedAnimation = CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+    tabController = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -74,92 +72,92 @@ class _HunterPubRouteState extends State with TickerProviderStateMixin {
         child: Overlay(
       initialEntries: [
         OverlayEntry(builder: (context) {
-          return DefaultTabController(
-              length: 4,
-              child: Scaffold(
-                appBar: AppBar(
-                  leading: IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.of(context).maybePop();
-                    },
-                    padding: const EdgeInsets.all(16),
-                  ),
-                  title: Builder(builder: (context) {
-                    DefaultTabController.of(context)?.index = hunterRoundTables.keys.toList().indexOf(currentSelectedTab);
-                    return TabBar(
-                      isScrollable: true,
-                      tabs: hunterRoundTables.keys
-                          .map((key) => Tab(
-                            height: context
-                                .findAncestorWidgetOfExactType<AppBar>()!
-                                .preferredSize
-                                .height -
-                                2.0,
-                            child: PubTab(
-                                onClick: () {
-                                  setState(() {
-                                    currentSelectedTab = key;
-                                  });
-                                },
-                                imageUrl: "assets/images/$key.png",
-                                content: hunterRoundTables[key]!),
-                          ))
-                              .toList(),
-                      labelStyle: const TextStyle(fontWeight: FontWeight.bold),
-                      unselectedLabelStyle: const TextStyle(
-                          color: Color.fromRGBO(203, 203, 207, 0.5)),
-                    );
-                  }),
-                  actions: [
-                    Builder(builder: (context) {
-                      return IconButton(
-                          onPressed: () {
-                            setState(() {
-                              final size = MediaQuery.of(context).size;
-                              final _bannerHeight = context
-                              !.findAncestorWidgetOfExactType<
-                                  AppBar>()
-                              !.preferredSize
-                                  .height;
-                              bannerHeight = _bannerHeight;
-                              bool nextExpanded = !expanded;
-                              arrowAnimation = Tween(begin: 0.0, end: math.pi).animate(curvedAnimation)..addListener(() {setState(() {
-                              });});
-                              overlayAnimation = Tween(begin: 0.0, end: size.height-(bannerHeight ?? 0)).animate(curvedAnimation)..addListener(() {setState(() {
-                              });});
-                              if (!expanded) {
-                                animationController.forward();
-                              } else {
-                                animationController.reverse();
-                              }
-                              expanded = nextExpanded;
-                            });
-                          },
-                          icon: Container(
-                              alignment: Alignment.center,
-                              child: Transform.rotate(
-                                angle: arrowAnimation is Animation ? arrowAnimation!.value : 0,
-                                child: const Icon(Icons.arrow_downward),
-                              )
-                          ));
-                    })
-                  ],
-                ),
-                body: TabBarView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      MercariHunter(),
-                      Text("123"),
-                      Text("123"),
-                      Text("123"),
-                    ]),
-                floatingActionButton: FloatingActionButton(
-                  tooltip: 'todo',
-                  onPressed: () {},
-                  child: const Icon(Icons.add),
-                ),
-              ));
+          return Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).maybePop();
+                },
+                padding: const EdgeInsets.all(16),
+              ),
+              title: Builder(builder: (context) {
+                return TabBar(
+                  controller: tabController,
+                  isScrollable: true,
+                  onTap: (idx) {
+                    tabController.index = idx;
+                    setState(() {
+                      currentSelectedTab = hunterRoundTables.keys.toList()[idx];
+                    });
+                  },
+                  tabs: hunterRoundTables.keys
+                      .map((key) => Tab(
+                    height: context
+                        .findAncestorWidgetOfExactType<AppBar>()!
+                        .preferredSize
+                        .height -
+                        2.0,
+                    child: PubTab(
+                        imageUrl: "assets/images/$key.png",
+                        content: hunterRoundTables[key]!),
+                  ))
+                      .toList(),
+                  labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+                  unselectedLabelStyle: const TextStyle(
+                      color: Color.fromRGBO(203, 203, 207, 0.5)),
+                );
+              }),
+              actions: [
+                Builder(builder: (context) {
+                  return IconButton(
+                      onPressed: () {
+                        setState(() {
+                          final size = MediaQuery.of(context).size;
+                          final _bannerHeight = context
+                          !.findAncestorWidgetOfExactType<
+                              AppBar>()
+                          !.preferredSize
+                              .height;
+                          bannerHeight = _bannerHeight;
+                          bool nextExpanded = !expanded;
+                          arrowAnimation = Tween(begin: 0.0, end: math.pi).animate(curvedAnimation)..addListener(() {setState(() {
+                          });});
+                          overlayAnimation = Tween(begin: 0.0, end: size.height-(bannerHeight ?? 0)).animate(curvedAnimation)..addListener(() {setState(() {
+                          });});
+                          if (!expanded) {
+                            animationController.forward();
+                          } else {
+                            animationController.reverse();
+                          }
+                          expanded = nextExpanded;
+                        });
+                      },
+                      icon: Container(
+                          alignment: Alignment.center,
+                          child: Transform.rotate(
+                            angle: arrowAnimation is Animation ? arrowAnimation!.value : 0,
+                            child: const Icon(Icons.arrow_downward),
+                          )
+                      ));
+                })
+              ],
+            ),
+            body: TabBarView(
+                controller: tabController,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  MercariHunter(),
+                  Text("123"),
+                  Text("123"),
+                  Text("123"),
+                ]),
+            floatingActionButton: FloatingActionButton(
+              tooltip: 'todo',
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+          );
         }),
         OverlayEntry(builder: (context) {
           return Positioned(
@@ -191,8 +189,9 @@ class _HunterPubRouteState extends State with TickerProviderStateMixin {
                             active: currentSelectedTab == hunterKey,
                             title: hunterRoundTables[hunterKey]!,
                             setTitle: () {
+                              tabController.index = hunterRoundTables.keys.toList().indexOf(hunterKey);
                               setState(() {
-                                currentSelectedTab=hunterKey;
+                                currentSelectedTab = hunterKey;
                               });
                             }
                         )).toList(),
